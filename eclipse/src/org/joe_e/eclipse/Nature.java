ec47954fd9d8a0d77c5262e10f7fca24631a4991
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2006-03-25 02:55:21 +0000

    Added BSD Licensing.

--- eclipse/src/org/joe_e/eclipse/Nature.java
+++ eclipse/src/org/joe_e/eclipse/Nature.java
@@ -1,3 +1,8 @@
+// Copyright 2005-06 Regents of the University of California.  May be used 
+// under the terms of the revised BSD license.  See LICENSING for details.
+/** 
+ * @author Adrian Mettler 
+ */
 package org.joe_e.eclipse;
 
 import org.eclipse.core.resources.ICommand;
