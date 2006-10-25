c8fdc2754f2ae621d375d6c6c84697e514dd6d16
daw <daw@5e140aaa-b445-0410-866e-67f42e7aabd9>
2006-10-25 06:07:22 +0000

    Fix spec of what Selfless means.

--- trunk/library/src/org/joe_e/Selfless.java
+++ trunk/library/src/org/joe_e/Selfless.java
@@ -9,23 +9,20 @@ package org.joe_e;
 
 /**
  * Marker interface for annotating classes that are indistinguishable from
- * a shallow copy of themselves, and are transparently copyable.
+ * a shallow copy of themselves.
  * 
  * <P>Joe-E requires that classes that implement this interface must
  * meet all of the following obligations:
  * <OL>
- * <LI> All instance fields must be public and final.
+ * <LI> All instance fields must be final.
  * <LI> The class cannot be equatable.
  * <LI> The object identity of instances of the class is not visible.  This
  *   can be satisfied by one of:
  *   <OL>
- *   <LI> The superclass implements Record; or,
+ *   <LI> The superclass implements Selfless; or,
  *   <LI> The class overrides <CODE>hashCode()</CODE> and <CODE>equals()</CODE>,
  *       and doesn't call <CODE>super.hashCode()</CODE> or <CODE>super.equals()</CODE>. 
  *   </OL>
- * <LI> A trivial constructor is provided (one that is passed values for
- * the fields, directly assign parameters to the newly constructed object's
- * fields, and does nothing else).
  * </OL>
  *  
  * <P>This interface contains no members.
