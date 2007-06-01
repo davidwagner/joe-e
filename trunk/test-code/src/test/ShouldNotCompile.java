4cee4fcb3bf337c187c1d090a6ea82a6d54827b1
adrian <adrian@5e140aaa-b445-0410-866e-67f42e7aabd9>
2007-06-01 03:10:18 +0000

    Describe Constructor/final fields bug in comment and link to Eclipse bug report.

--- trunk/test-code/src/test/ShouldNotCompile.java
+++ trunk/test-code/src/test/ShouldNotCompile.java
@@ -1,5 +1,14 @@
-package test.verifier;
-
+package test;
+/*
+ * This file should not compile; Sun's compiler and GCJ both refuse to compile
+ * it.  Eclipse's compiler (ECJ) compiles it without complaint, generating
+ * bytecode that violates the semantics of final fields.
+ * 
+ * The JVM does not check for this kind of thing.
+ *
+ * I've filed an Eclipse bug on this.
+ * https://bugs.eclipse.org/bugs/show_bug.cgi?id=190391
+ */
 public class ShouldNotCompile {
 	final int contents;
 	
