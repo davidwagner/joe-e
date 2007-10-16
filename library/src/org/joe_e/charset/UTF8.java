1ca8e388b80f17567a2eebd0bffb3e92848f2d81
adrian <adrian@5e140aaa-b445-0410-866e-67f42e7aabd9>
2007-10-16 06:22:11 +0000

    explicitly check for overflow in ByteArray.Generator.write
    add one-argument decode to ASCII and UTF8

--- library/src/org/joe_e/charset/UTF8.java
+++ library/src/org/joe_e/charset/UTF8.java
@@ -31,7 +31,23 @@ public final class UTF8 {
     /**
      * Decodes a UTF-8 string. Each byte not corresponding to a UTF-8
      * character decodes to the Unicode replacement character U+FFFD.
+     * Note that an initial byte-order mark is not stripped.  This method is
+     * equivalent to <code>decode(buffer, 0, buffer.length)</code>.
+     * @parameter buffer    the ASCII-encoded string to decode
+     * @return The corresponding string
+     * @throws java.lang.IndexOutOfBoundsException
+     */
+    static public String decode(byte[] buffer) {
+        return decode(buffer, 0, buffer.length);
+    }
+    
+    /**
+     * Decodes a UTF-8 string. Each byte not corresponding to a UTF-8
+     * character decodes to the Unicode replacement character U+FFFD.
      * Note that an initial byte-order mark is not stripped.
+     * @parameter buffer    the ASCII-encoded string to decode
+     * @parameter off       where to start decoding
+     * @parameter len       how many bytes to decode
      * @return The corresponding string
      * @throws java.lang.IndexOutOfBoundsException
      */
