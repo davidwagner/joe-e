4382f9b35a3143ebf1b118e1282b56735d9f99ca
adrian <adrian@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-02-04 06:10:47 +0000

    Added tests for fields of parameter type in Immutable and Powerless classes

---
+++ trunk/test-code/src/unittest/pass/ImmutableWithFields.java
@@ -0,0 +1,10 @@
+package unittest.pass;
+
+import org.joe_e.Immutable;
+
+public class ImmutableWithFields<E extends Immutable & Iterable>
+										implements Immutable {
+	final E ee = null;
+	final Exception e = null;
+	final String s = null;
+}
