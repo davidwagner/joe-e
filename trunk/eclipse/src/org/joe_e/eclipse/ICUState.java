bbe4336ca19a143a1b64672f774554e149435b57
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-02-13 08:58:41 +0000

    bumped version to 2.0.0
    included license, copyright dates for all java files

--- trunk/eclipse/src/org/joe_e/eclipse/ICUState.java
+++ trunk/eclipse/src/org/joe_e/eclipse/ICUState.java
@@ -1,9 +1,13 @@
+// Copyright 2005-06 Regents of the University of California.  May be used 
+// under the terms of the revised BSD license.  See LICENSING for details.
+/** 
+ * @author Adrian Mettler 
+ */
 package org.joe_e.eclipse;
 
 import java.util.Set;
 import java.util.HashSet;
 import org.eclipse.jdt.core.IType;
-
 
 /**
  * Information determined about a class by the verifier
