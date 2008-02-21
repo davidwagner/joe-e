471d326bd15bd101d97b6fa6fe5897abac4317a5
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-02-21 08:53:29 +0000

    - make safej-library an eclipse project - minor refactor of SafeJParse: each file parsed is an instance, rather than resetting instance variables per-file.

--- safej-library/src/org/joe_e/safej/SafeJParse.java
+++ safej-library/src/org/joe_e/safej/SafeJParse.java
@@ -1,4 +1,4 @@
-// Copyright 2007 Regents of the University of California.  May be used 
+// Copyright 2007-08 Regents of the University of California.  May be used 
 // under the terms of the revised BSD license.  See LICENSING for details.
 /** 
  * @author Adrian Mettler 
@@ -24,74 +24,73 @@ public class SafeJParse {
     static class ParseFailure extends Exception {
         static final long serialVersionUID = 1;
     }
-       
-    File dir;
-    String currentFile;
+
+    final String filename;
+    final PrintStream err;
+    final SafeJConsumer consumer;
+    
+    final List<String> honoraries = new ArrayList<String>();
+    final List<Member> staticMembers = new ArrayList<Member>();
+    final List<Member> instanceMembers = new ArrayList<Member>();
+
     StreamTokenizer st;
     int nextToken;
-    PrintStream err;
-    SafeJConsumer consumer;
-    
     String comment = null; // the class comment
-    List<String> honoraries = new ArrayList<String>();
-    List<Member> staticMembers = new ArrayList<Member>();
-    List<Member> instanceMembers = new ArrayList<Member>();
     
-    public SafeJParse(File dir, PrintStream err, SafeJConsumer consumer) {
+    public SafeJParse(File f, PrintStream err, SafeJConsumer consumer) {
         this.err = err;
-        this.dir = dir;
         this.consumer = consumer;
-        process(dir);
-    }
-    
-    private void process(final File f) {
+        this.filename = f.getName();
         if (f.isDirectory()) {
             for (File child : f.listFiles()) {
-                process(child);
+                new SafeJParse(child, err, consumer);
             }
-        } else if (f.getName().endsWith(".safej")) {
-            honoraries.clear();
-            staticMembers.clear();
-            instanceMembers.clear();
+        } else if (filename.endsWith(".safej")){
+            parse(f);
+        }
+    }
+    
+    /**
+     * Parse a safej file, invoking the safej consumer
+     * @param f a safej file
+     */
+    private void parse(final File f) {
+        try {
+            FileReader in = new FileReader(f);
+            st = new StreamTokenizer(in);             
+            st.commentChar('#');
+            st.quoteChar('"');
+            nextToken = st.nextToken();
             
-            currentFile = f.getName();
-            try {
-                FileReader in = new FileReader(f);
-                st = new StreamTokenizer(in);             
-                st.commentChar('#');
-                st.quoteChar('"');
-                nextToken = st.nextToken();
-                
-                boolean safe = true;
-                
-                // Parse mandatory components of safej file
-                assertNextWordIs("class");
-                assertNextTokenIs('(');
-                if (nextWordIs("safe")) {
-                    assertNextTokenIs(',');
-                } else if (nextWordIs("unsafe")) {
-                    assertNextTokenIs(',');
-                    safe = false;
-                }
-                String className = getNextString();
-                
-                // Parse optional components
-                parseBody();
-                
-                assertNextTokenIs(')');
-                if (safe) {
-                    consumer.consumeClass(className, comment, honoraries, 
-                                          staticMembers, instanceMembers);
-                } else {
-                    consumer.disabledClass(className, comment);
-                }             
-            } catch (FileNotFoundException fnfe) {
-                fnfe.printStackTrace(err);
-            } catch (IOException ioe) {
-                ioe.printStackTrace(err);
-            } catch (ParseFailure pf) {
+            boolean safe = true;
             
+            // Parse mandatory components of safej file
+            assertNextWordIs("class");
+            assertNextTokenIs('(');
+            if (nextWordIs("safe")) {
+                assertNextTokenIs(',');
+            } else if (nextWordIs("unsafe")) {
+                assertNextTokenIs(',');
+                safe = false;
             }
+            String className = getNextString();
+                
+            // Parse optional components
+            parseBody();
+               
+            assertNextTokenIs(')');
+            if (safe) {
+                consumer.consumeClass(className, comment, honoraries, 
+                                      staticMembers, instanceMembers);
+            } else {
+                consumer.disabledClass(className, comment);
+            }             
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace(err);
+        } catch (IOException ioe) {
+            ioe.printStackTrace(err);
+        } catch (ParseFailure pf) {
+         
         }
     }
     
@@ -200,7 +199,7 @@ public class SafeJParse {
     }
     
     private void failParsing(String expectation) throws ParseFailure {
-        err.println(currentFile + ":" + st.lineno() + ": " + expectation);
+        err.println(filename + ":" + st.lineno() + ": " + expectation);
         throw new ParseFailure();
     }
     
