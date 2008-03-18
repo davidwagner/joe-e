d146ffac9384940740bf01c4b22f359544f412bb
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-03-18 04:46:04 +0000

    Fix spacing in Problem.java Change to have Joe-E problem marker not be a subclass of Java marker (this had a weird effect when package-info.java had Joe-E errors, and made eclipse warn of compilation errors when running/debugging the project)

--- eclipse/src/org/joe_e/eclipse/Problem.java
+++ eclipse/src/org/joe_e/eclipse/Problem.java
@@ -15,48 +15,48 @@ public class Problem {
     private final int severity;	// uses IMarker severity levels
 	
     public Problem(String message) {
-	this.message = message;
-	this.start = 0;
-	this.length = 0;
-	this.severity = IMarker.SEVERITY_ERROR;
+        this.message = message;
+        this.start = 0;
+        this.length = 0;
+        this.severity = IMarker.SEVERITY_ERROR;
     }
 	
     public Problem(String message, int severity) {
-	this.message = message;
-	this.start = 0;
-	this.length = 0;
-	this.severity = severity;
+        this.message = message;
+        this.start = 0;
+        this.length = 0;
+        this.severity = severity;
     }
 	
     public Problem(String message, ISourceRange location)
     {
-	this.message = message;
-	this.start = location.getOffset();
-	this.length = location.getLength();
-	this.severity = IMarker.SEVERITY_ERROR;
+        this.message = message;
+        this.start = location.getOffset();
+        this.length = location.getLength();
+        this.severity = IMarker.SEVERITY_ERROR;
     }
 	
     public Problem(String message, int start, int length)
     {
-	this.message = message;
-	this.start = start;
-	this.length = length;
-	this.severity = IMarker.SEVERITY_ERROR;
+        this.message = message;
+        this.start = start;
+        this.length = length;
+        this.severity = IMarker.SEVERITY_ERROR;
     }
 	
     public String getMessage() {
-	return message;
+        return message;
     }
 	
     public int getSeverity() {
-	return severity;
+        return severity;
     }
     
     public int getStart() {
-	return start;
+        return start;
     }
 	
     public int getEnd() {
-	return start + length;
+        return start + length;
     }
 }
