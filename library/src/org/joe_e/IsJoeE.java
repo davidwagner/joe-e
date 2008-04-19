f0f6a657384a1bbbb2f10fc774b8e1a8ff2005a1
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-04-19 05:58:30 +0000

    close parentheses in javadoc comment for IsJoeE

--- library/src/org/joe_e/IsJoeE.java
+++ library/src/org/joe_e/IsJoeE.java
@@ -10,7 +10,7 @@ import java.lang.annotation.*;
  * Package annotation to use for packages containing Joe-E code. All classes in
  * the package will be checked if this annotation is applied. (Package 
  * annotations can be specified in a file named <code>package-info.java</code>
- * with a line like "<code>@org.joe_e.IsJoeE package package.name;</code>".
+ * with a line like "<code>@org.joe_e.IsJoeE package package.name;</code>".)
  */
 @Target(ElementType.PACKAGE)
 @Retention(RetentionPolicy.RUNTIME)
