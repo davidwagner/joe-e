ccfe6c606d1985047542ddb579082163bda6d959
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-09-25 09:12:18 +0000

    added some tests for n-ary (non-varargs) array() methods
    renamed DisabledIterator* to more precise DisabledIterable*

--- test-code/src/unittest/fail/DisabledIterable1.java
+++ test-code/src/unittest/fail/DisabledIterable1.java
@@ -1,10 +1,8 @@
 package unittest.fail;
 
-import org.joe_e.testlib.DisabledIterator;
-
-public class DisabledIterator2 {
+public class DisabledIterable1 {
     public void foo() {
-    	DisabledIterator<String> i = null;
+    	Iterable<String> i = null;
     	
         for (String s : i) {
         	s.length();
