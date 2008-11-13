c02a225be31d340c4c8745093926249068bd0283
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2008-11-13 23:00:57 +0000

    fix one test case to only generate one Joe-E error; provide some documentation on test cases in package-info files.

--- trunk/test-code/src/unittest/fail/UnimplementedHonorary3.java
+++ trunk/test-code/src/unittest/fail/UnimplementedHonorary3.java
@@ -2,7 +2,7 @@ package unittest.fail;
 
 import org.joe_e.testlib.HasHonorary2;
 
-public class UnimplementedHonorary3 {
+public class UnimplementedHonorary3 implements org.joe_e.Immutable {
 	void foo() {
 		new HasHonorary2(new Object()) {
 			static final long serialVersionUID = 1;
--- trunk/test-code/src/unittest/fail/package-info.java
+++ trunk/test-code/src/unittest/fail/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * Every file in this package except for this one should have one error.  Use
+ * the pigeonhole principle (every file should have an error and the total
+ * number of errors should equal the number of files minus one).  Hooray for
+ * discrete mathematics!
+ */
+
 @org.joe_e.IsJoeE package unittest.fail;
