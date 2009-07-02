3b135d44459c66d87cf485f112859d9ddc308d5e
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2009-07-02 07:48:56 +0000

    add testlib classes for additional unit tests on interface implementation detection

---
+++ trunk/test-library/org/joe_e/testlib/DisabledMethodTakesString.java
@@ -0,0 +1,11 @@
+package org.joe_e.testlib;
+
+public class DisabledMethodTakesString {
+    public DisabledMethodTakesString() {
+        
+    }
+    
+    public void takesString(String s) {
+        
+    }
+}
