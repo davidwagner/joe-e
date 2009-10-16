e8cdc3b2902f841495fe3b409a047ddd0ba6d843
krish.akshay <krish.akshay@5e140aaa-b445-0410-866e-67f42e7aabd9>
2009-10-16 18:59:29 +0000

    update some of the performance experiment scripts

--- apps/servlet/test/python/microbenchmark.py
+++ apps/servlet/test/python/microbenchmark.py
@@ -1,8 +1,9 @@
 import mechanize
 import cookielib
 import time
+import os
 
-URLBASE = "http://boink.cs.berkeley.edu:8080/"
+URLBASE = "http://localhost:8080/"
 
 def runIteration(app, username):
     br = mechanize.Browser()
@@ -15,18 +16,31 @@ def runIteration(app, username):
     for link in br.links():
         if link.text == "Log In":
             res = br.follow_link(link=link)
-            if br.viewing_html():
-                br.select_form(nr=0)
-                br.form['username'] = username
-                br.form['password'] = username
-                br.submit()
+#             if br.viewing_html():
+#                 br.select_form(nr=0)
+#                 br.form['username'] = username
+#                 br.form['password'] = username
+#                 br.submit()
 
+
+print os.popen("/Users/akshay/bin/stop_tomcat").read()
+time.sleep(5)
+print os.popen("/Users/akshay/bin/start_tomcat").read()
+time.sleep(30)
+print "Starting memory monitor... "
+os.popen("java -cp /Users/akshay/Documents/workspace/servlet/bin/ org.joe_e.servlet.test.JMXMonitor > pmemoryusage.out &")
+time.sleep(60)
+print "Starting experiment... "
 totalstart = time.time()
-for k in range(0, 500, 10):
+for k in range(0, 15000, 10):
     start = time.time()
     for i in range(k, k+10):
-        runIteration("servlet", "p"+str(i))
+        runIteration("perf", "p"+str(i))
     stop = time.time()
     print "%d %0.3f" % (k, stop-start)
 totalstop = time.time()
 print "Total %0.3f" % (totalstop - totalstart)
+
+while True:
+    time.sleep(10)
+
