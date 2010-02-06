ab1497b5f4f9b9f047b784b6852d6b56597dcb8c
krish.akshay <krish.akshay@5e140aaa-b445-0410-866e-67f42e7aabd9>
2010-02-06 09:13:07 +0000

    throughput tests are working now

--- apps/servlet/test/python/multithreaded.py
+++ apps/servlet/test/python/multithreaded.py
@@ -1,12 +1,17 @@
-import threading
+import threading, re
 import time
-import os
-import urllib2
+import os, sys
+import urllib, urllib2
 
 URLBASE = "http://localhost:8080/"
 SLEEPTIME = 180
 BASEDIR = "throughput/"
 APPNAME = "servlet/"
+
+cookieProcessor = urllib2.HTTPCookieProcessor()
+opener = urllib2.build_opener(cookieProcessor)
+urllib2.install_opener(opener)
+reg = re.compile("hidden\" value=\"([-0-9a-f]*)\"")
 
 done = False
 class SThread(threading.Thread):
@@ -18,26 +23,37 @@ class SThread(threading.Thread):
     def run(self):
 	self.arr = []
         while not done:
-            try:
-		urllib2.urlopen(URLBASE+APPNAME)
-		urllib2.urlopen(URLBASE+APPNAME+"login")
-                stop = time.time()
+	    try:
+		trace(self.i)
+		stop = time.time()
 		self.arr.append(str(stop))
-            except Exception:
-                print "error: " + str(time.time())
-	output = reduce(lambda x,y: str(x) + "\n" + str(y), self.arr)
+	    except Exception as ex:
+		pass
+	output = reduce(lambda x,y: str(x) + "\n" + str(y), self.arr, "")
 	self.file.write(output)
 	self.file.flush()
+	print "thread %d is done" % (self.i)
 
-for kk in [10]:
-    for iters in range(1):
-	for jj in ["servlet"]:
+def trace(i):
+	urllib2.urlopen(URLBASE+APPNAME, None, 10)
+	response = urllib2.urlopen(URLBASE+APPNAME+"login", None, 10)
+#	text = reduce(lambda x,y: x+ " " + y, response.readlines())
+#	m = reg.search(text)
+#	token = m.group(1)
+#	data = urllib.urlencode({"username": "p"+str(i), "password": "p"+str(i), "__joe-e__csrftoken" : token})
+#	response = urllib2.urlopen(URLBASE+APPNAME+"login", data, 5)
+	urllib2.urlopen(URLBASE+APPNAME+"logout", None, 10)
+
+
+for kk in [20]:
+    for iters in range(20): #0):
+	for jj in ["servlet", "perf"]:
 	    os.mkdir(BASEDIR+jj+str(kk)+"-"+str(iters))
-# 	    os.popen("/usr/lib/apache-tomcat/bin/shutdown.sh").read()
-# 	    time.sleep(5)
-# 	    os.popen("/usr/lib/apache-tomcat/bin/startup.sh").read()
-# 	    time.sleep(15)
-            urllib2.urlopen("http://www.google.com")
+	    os.popen("/usr/lib/apache-tomcat/bin/shutdown.sh").read()
+	    time.sleep(5)
+	    os.popen("/usr/lib/apache-tomcat/bin/startup.sh").read()
+	    time.sleep(10)
+
 	    print "Starting experiment ... "
 	    print "*** Running " + jj + " with " + str(kk) + "threads ***"
 	
@@ -51,7 +67,7 @@ for kk in [10]:
 		sthreads[i].start()
 
 	    time.sleep(SLEEPTIME)
-	    
+	    print "joining threads"
 	    done = True
 	    
 	    for i in range(kk):
