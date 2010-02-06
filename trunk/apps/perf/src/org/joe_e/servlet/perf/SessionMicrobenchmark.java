2458ba05571e7aab53194bf3e4aa00142a11d4fe
krish.akshay <krish.akshay@5e140aaa-b445-0410-866e-67f42e7aabd9>
2010-02-06 09:40:40 +0000

    added session microbenchmarks

---
+++ trunk/apps/perf/src/org/joe_e/servlet/perf/SessionMicrobenchmark.java
@@ -0,0 +1,14 @@
+package org.joe_e.servlet.perf;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionMicrobenchmark extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	}
+}
