2bb99b3ae6b95e7e3f5ace4dc0d0b647a4d437b4
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2010-04-10 09:06:31 +0000

    updated instructions/shell script that never made their way into version control

--- eclipse-extras/verify.sh
+++ eclipse-extras/verify.sh
@@ -1,15 +1,22 @@
-#! /bin/sh
+#!/bin/sh
 
-# you will need to fill out these variables with: 
-# the full path to your eclipse plugins directory
+# You will need to fill out these variables with: 
+# The full path to your eclipse plugins directory (the location of the file
+#   org.eclipse.equinox.launcher.<someversion>.jar)
 # The full path to your taming database
-# the full path to your joe-e library
+# the full path to your Joe-E library (in the format of a classpath entry,
+#   e.g. a JAR file)
 # before you run the command line verifier
 
-ECLIPSE_PLUGINS_DIR="/Applications/eclipse/plugins/"
-TAMING_DIR="/Users/akshay/School/Research/Joe-e/taming/"
-LIBRARY_JAR="/Users/akshay/Downloads/library-2.1.0.jar"
+ECLIPSE_PLUGINS_DIR=""
+TAMING_DIR=""
+LIBRARY_CLASSPATH=""
 
-LAUNCHER=`ls $ECLIPSE_PLUGINS_DIR | grep -i org.eclipse.equinox.launcher.*\.jar`
-CMD="java -cp $ECLIPSE_PLUGINS_DIR -jar $ECLIPSE_PLUGINS_DIR/$LAUNCHER -application org.joe_e.Main --taming $TAMING_DIR --library $LIBRARY_JAR $@"
-$CMD
+CLASSPATH=$LIBRARY_CLASSPATH:$CLASSPATH
+
+#LAUNCHER=`ls $ECLIPSE_PLUGINS_DIR | grep -i org.eclipse.equinox.launcher.*\.jar`
+java -cp $ECLIPSE_PLUGINS_DIR \
+     -jar $ECLIPSE_PLUGINS_DIR/org.eclipse.equinox.launcher.*.jar \
+     -application org.joe_e.Main \
+     -taming $TAMING_DIR -classpath $CLASSPATH "$@"
+
