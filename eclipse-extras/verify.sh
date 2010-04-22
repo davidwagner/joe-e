7789d0f74b2e7405d9922a664c472b18fcc6ee77
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2010-04-22 05:21:12 +0000

    up-to-date and then more revised versions of verify script and install instructions

--- eclipse-extras/verify.sh
+++ eclipse-extras/verify.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
 # You will need to fill out these variables with: 
-# The full path to your eclipse plugins directory (the location of the file
-#   org.eclipse.equinox.launcher.<someversion>.jar)
-# The full path to your taming database
-# the full path to your Joe-E library (in the format of a classpath entry,
-#   e.g. a JAR file)
-# before you run the command line verifier
+# *  The full path to your eclipse plugins directory (the location of the file
+#      org.eclipse.equinox.launcher.<someversion>.jar)
+# *  The full path to your taming database
+# *  The full path to your Joe-E library (in the format of a classpath entry,
+#      e.g. a JAR file)
+# before you can run the command-line verifier
+# Do NOT put double quotes around these values!  To escape a space, use "\ "
 
-ECLIPSE_PLUGINS_DIR=""
-TAMING_DIR=""
-LIBRARY_CLASSPATH=""
+ECLIPSE_PLUGINS_DIR=~/proj/eclipse/plugins
+TAMING_DIR=~/proj/joe-e/commandline-2.2.2/taming-20100421
+LIBRARY_CLASSPATH=~/proj/joe-e/commandline-2.2.2/library-2.2.2.jar
 
+# The remaining lines should be OK as is.  Note that the CLASSPATH
+# environment variable is used to specify the location of additional
+# libraries needed to compile the source code being verified.
 CLASSPATH=$LIBRARY_CLASSPATH:$CLASSPATH
+LAUNCHER_PATH=`echo $ECLIPSE_PLUGINS_DIR/org.eclipse.equinox.launcher*.jar`
 
-#LAUNCHER=`ls $ECLIPSE_PLUGINS_DIR | grep -i org.eclipse.equinox.launcher.*\.jar`
-java -cp $ECLIPSE_PLUGINS_DIR \
-     -jar $ECLIPSE_PLUGINS_DIR/org.eclipse.equinox.launcher.*.jar \
+echo java -cp "$ECLIPSE_PLUGINS_DIR" \
+     -jar "$LAUNCHER_PATH" \
      -application org.joe_e.Main \
-     -taming $TAMING_DIR -classpath $CLASSPATH "$@"
+     -taming "$TAMING_DIR" -classpath "$CLASSPATH" "$@"
+
+java -cp "$ECLIPSE_PLUGINS_DIR" \
+     -jar "$LAUNCHER_PATH" \
+     -application org.joe_e.Main \
+     -taming "$TAMING_DIR" -classpath "$CLASSPATH" "$@"
 
