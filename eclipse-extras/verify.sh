672422f55f3483fe301cc511d28996bbf63b449f
amettler <amettler@5e140aaa-b445-0410-866e-67f42e7aabd9>
2011-06-15 07:25:31 +0000

    update version, fix for if eclipse library path has spaces

--- eclipse-extras/verify.sh
+++ eclipse-extras/verify.sh
@@ -6,18 +6,18 @@
 # *  The full path to your taming database
 # *  The full path to your Joe-E library (in the format of a classpath entry,
 #      e.g. a JAR file)
-# before you can run the command-line verifier
-# Do NOT put double quotes around these values!  To escape a space, use "\ "
+# before you can run the command-line verifier.
+# Escape any spaces with "\ " instead of using quotes.
 
 ECLIPSE_PLUGINS_DIR=~/proj/eclipse/plugins
-TAMING_DIR=~/proj/joe-e/commandline-2.2.2/taming-20100421
-LIBRARY_CLASSPATH=~/proj/joe-e/commandline-2.2.2/library-2.2.2.jar
+TAMING_DIR=~/proj/joe-e/commandline-2.2.3/taming-20100421
+LIBRARY_CLASSPATH=~/proj/joe-e/commandline-2.2.3/library-2.2.3.jar
 
 # The remaining lines should be OK as is.  Note that the CLASSPATH
 # environment variable is used to specify the location of additional
 # libraries needed to compile the source code being verified.
 CLASSPATH=$LIBRARY_CLASSPATH:$CLASSPATH
-LAUNCHER_PATH=`echo $ECLIPSE_PLUGINS_DIR/org.eclipse.equinox.launcher*.jar`
+LAUNCHER_PATH=`echo "$ECLIPSE_PLUGINS_DIR"/org.eclipse.equinox.launcher_*.jar`
 
 echo java -cp "$ECLIPSE_PLUGINS_DIR" \
      -jar "$LAUNCHER_PATH" \
