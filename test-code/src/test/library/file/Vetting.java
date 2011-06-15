fd5d64cfd24b6b542dd7b96ac327578b9b9b331d
amettler <amettler@gmail.com>
2011-06-15 00:24:52 +0000

    add test cases for code to reject null bytes in file name, and for backslashes in file names (only tests behavior on current platform)

--- test-code/src/test/library/file/Vetting.java
+++ test-code/src/test/library/file/Vetting.java
@@ -1,5 +1,7 @@
 package test.library.file;
 
+import java.io.File;
+
 import org.joe_e.file.Filesystem;
 import org.joe_e.file.InvalidFilenameException;
 
@@ -8,7 +10,9 @@ public class Vetting {
         String[] pass = {"ok", "longish alphanumeric", "didn't", "*\"funky",
                          "*.*", "jeepers", "stuff..", "ork.ext", ".hack", 
                          "hello..good-bye"};
-        String[] fail = {"yes/no", ".", "..", "../asdf", "T/Y", "/join"};
+        String[] fail = {"yes/no", ".", "..", "../asdf", "T/Y", "/join", "..\0", "\0", ".\0",
+                         "..\0ok", "\0\1\2", "\0../.."};
+        String[] withBackslashes = {"\\", "yes\\no", "..\\asdf", "T\\Y", "\\join"};
         
         for (String p : pass) {
             Filesystem.checkName(p);
@@ -22,5 +26,15 @@ public class Vetting {
             
             }
         }
+        
+        for (String b : withBackslashes) {
+            try {
+                Filesystem.checkName(b);
+                assert File.pathSeparatorChar != '\\';
+            } catch (InvalidFilenameException ife) {
+                assert File.pathSeparatorChar == '\\';
+            }
+        }
+        
     }
 }
